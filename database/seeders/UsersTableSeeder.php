8ac35d1fe252b04b91a13bae8ad5019b82ce72cd
Jesse Orange <jesse.orange@newable.co.uk>
2022-07-08 16:52:07 +0100

    Setup Laravel Sanctum

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Jesse Orange',
+            'email' => 'jesseorange360@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
